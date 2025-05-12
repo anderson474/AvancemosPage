57d308b30bc7a59b781e6dc7b5b34668e90a8ed0
Jhon Anderson galeano <jhon.galeano201@tau.usbmed.edu.co>
2025-05-12 09:22:49 -0500

    Mejora en la carga de los slider de gallery con lazy

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,9 +1,13 @@
 // Galeria.jsx
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination } from 'swiper/modules';
+import { Autoplay, Pagination} from 'swiper/modules';
+
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
+
+
+import Image from 'next/image';
 
 const Galeria = () => {
   return (
@@ -13,22 +17,74 @@ const Galeria = () => {
         autoplay={{ delay: 3000, disableOnInteraction: false }}
         pagination={{ clickable: true }}
         loop={true}
+        lazy={true}
         className="rounded-xl"
       >
         <SwiperSlide>
-          <img src="/galeria/Banner-educacion-inclusiva.png" alt="Foto 1" className="w-full h-auto rounded-xl" />
+          <div className="relative w-full h-auto">
+            <div className="swiper-lazy-preloader"></div>
+            <Image
+              src="/galeria/Banner-educacion-inclusiva.png"
+              alt="Foto 1"
+              width={1200}
+              height={600}
+              className="w-full h-auto rounded-xl swiper-lazy"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader"></div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/galeria/Banner-formacion-bilingue-02.png" alt="Foto 2" className="w-full h-auto rounded-xl" />
+          <div className="relative w-full h-auto">
+            <Image
+              src="/galeria/Banner-formacion-bilingue-02.png"
+              alt="Foto 2"
+              width={1200}
+              height={600}
+              className="w-full h-auto rounded-xl swiper-lazy"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader"></div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/galeria/Banner-formacion-continua.png" alt="Foto 3" className="w-full h-auto rounded-xl" />
+          <div className="relative w-full h-auto">
+            <Image
+              src="/galeria/Banner-formacion-continua.png"
+              alt="Foto 3"
+              width={1200}
+              height={600}
+              className="w-full h-auto rounded-xl swiper-lazy"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader"></div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/galeria/Banner-sistemas-de-evaluacion-03.png" alt="Foto 3" className="w-full h-auto rounded-xl" />
+          <div className="relative w-full h-auto">
+            <Image
+              src="/galeria/Banner-sistemas-de-evaluacion-03.png"
+              alt="Foto 4"
+              width={1200}
+              height={600}
+              className="w-full h-auto rounded-xl swiper-lazy"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader"></div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/galeria/Banners-evento-diplomado-02-1.png" alt="Foto 3" className="w-full h-auto rounded-xl" />
+          <div className="relative w-full h-auto">
+            <Image
+              src="/galeria/Banners-evento-diplomado-02-1.png"
+              alt="Foto 5"
+              width={1200}
+              height={600}
+              className="w-full h-auto rounded-xl swiper-lazy"
+              loading="lazy"
+            />
+            <div className="swiper-lazy-preloader"></div>
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
@@ -36,3 +92,5 @@ const Galeria = () => {
 };
 
 export default Galeria;
+
+
